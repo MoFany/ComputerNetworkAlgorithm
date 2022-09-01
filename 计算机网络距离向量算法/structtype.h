33f326fb7eca5e04be7b3872e01426bf0782354b
MoFany-J <63458509+MoFany@users.noreply.github.com>
2022-09-01 23:12:57 +0800

    Update structtype.h

--- 计算机网络距离向量算法/structtype.h
+++ 计算机网络距离向量算法/structtype.h
@@ -1,5 +1,5 @@
 #pragma once
-/*author:蒋明辉*/
+/*author:jiangminghui*/
 
 /*用户自定义头文件*/
 /*-------定义组成路由表结构体（中间加下划线的是自定义类型）-------*/
@@ -41,22 +41,3 @@ struct list2
 	route_type buffersedroute;		//临时路由表
 	route_type* temp;
 }bufferroutelist;//结构体变量(全局变量)
-
-
-//写一个结构体用于判断不同网络上路由的直接交付与间接交付情况
-//拓扑结构是图，则构造一个图，即：构造一个包含两个指针区域的结构体，再将其指针域指向不同节点（路由器），
-//设一个路由器有两个指针区域，用于接入不同的网络
-/*
-* 而网络又是一个结构体，网络结构体中没有距离的概念，网络结构体用于实现接入不同区域的路由器，
-* 也就是一个网络结构体（netstruct）中又包含多个路由器（routetype）
-* 不同网络之间通过某路由器进行链接
-* 
-* 
-* typedef struct nettype
-* {
-*	routetype *route[Maxsize];//一个网络默认包含Maxsize个路由器数组，每个数组元素都是一个指向路由器数据类型的指针变量
-*	nettype *netnext;
-* }nets;
-* 
-* 
-*/
